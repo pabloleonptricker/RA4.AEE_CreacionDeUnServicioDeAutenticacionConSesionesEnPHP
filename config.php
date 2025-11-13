8bcd7a4ffe593153dccbaac7ea0fb6192abb8240
Pablo.Leon <pabloleon.24@campuscamara.es>
2025-11-13 09:40:40 +0100

    definición de array de usuarios

--- config.php
+++ config.php
@@ -6,4 +6,11 @@
 
 //Concepto PHP Clave: 
 //Arrays (para usuarios), sesiones (session_start(), $_SESSION)
+
+//Definición de array de usuarios válidos:
+$usuarios = [
+    'carlosbasulto' => '1234',
+    'pavloleon' => '1111',
+    'admin' => 'admin'
+]
 ?>
