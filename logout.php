a80c8c9d2bec3df8d9c71be714638c8cef443035
Pablo.Leon <pabloleon.24@campuscamara.es>
2025-11-17 09:51:55 +0100

    desarrollo principal de logout

--- logout.php
+++ logout.php
@@ -5,4 +5,18 @@
 
 //Concepto PHP Clave: 
 //session_start(), session_destroy(), header() para redirección.
+
+//Incluir Configuración:
+require_once 'config.php';
+
+//Borrar las variables de sesión del array $_SESSION.
+$_SESSION = array(); 
+
+//Destruir la sesión en el servidor.
+
+session_destroy();
+
+//Redirigir al formulario de login para que el usuario pueda volver a entrar.
+header("Location: login.php");
+exit();
 ?>
